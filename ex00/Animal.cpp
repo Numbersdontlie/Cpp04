547060bdc881026cd0cc5d446a1b39b003d39c48
Numbersdontlie <luifer92@gmail.com>
2024-12-01 23:42:28 +0100

    starting the module 4

---
+++ ex00/Animal.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 23:24:23 by luifer            #+#    #+#             */
+/*   Updated: 2024/12/01 23:37:02 by luifer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Animal.hpp"
+
+//Default constructor
+Animal::Animal() : _type("Animal"){
+    std::cout << BLUE << " Animal from default constructor created" << RESET << std::endl;
+}
+
+//Copy constructor
+Animal::Animal(const Animal& input) : _type(input._type){
+    std::cout << BLUE << " Animal object from copy constructor created" << RESET << std::endl;
+}
+
+//Overload operator constructor
+Animal& Animal::operator=(const Animal& input){
+    if(this == &input)
+        return (*this);
+    _type = input._type;
+    return (*this);
+}
+
+//Destructor
+Animal::~Animal(){
+    std::cout << RED << "Animal" << this->_type << " Was destroyed" <<RESET << std::endl;
+}
+
+//Function to make sound
+void Animal::makeSound(){
+    std::cout << GREEN << " Animal sound" << RESET << std::endl;
+}
+
+//Getter of type
+std::string Animal::getType() const{return(this->type);}
---
+++ ex00/Dog.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luifer <luifer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 23:37:49 by luifer            #+#    #+#             */
+/*   Updated: 2024/12/01 23:41:54 by luifer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+# define DOG_HPP
+
+# include <iostream>
+# include <string>
+
+#define BLUE "\e[1;94m"
+#define RED "\e[1;91m"
+#define GREEN "\e[1;92m"
+#define RESET "\033[0m"
+
+class Dog : virtual public Animal
+{
+    public:
+        Dog();
+        Dog(const Dog& input);
+        Dog& operator=(const Dog& input);
+        ~Dog();
+
+        void makeSound();
+};
+
+#endif
